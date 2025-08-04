09d2f167789bb0f33814ec3032cba463960f8fc9
PeymonA <peymon.armand4@gmail.com>
2025-08-05 10:17:41 +1200

    Simple Header + Box component

--- mission0/src/App.jsx
+++ mission0/src/App.jsx
@@ -1,8 +1,29 @@
 import './App.css'
 
+const Box =({ word }) => {
+  return (
+    <div>
+      <p>{word}</p>
+    </div>
+  )
+}
+
 const App = () => {
   return(
-    <p>Hello</p>
+    <div>
+      <header>
+        <div>
+          <Box word= "My company"/>
+        </div>
+        <div>
+          <Box word= "Word"/>
+          <Box word= "Word"/>
+          <Box word= "Word"/>
+          <Box word= "Login"/>
+        </div>
+      </header>
+
+    </div>
   )
 }
 
